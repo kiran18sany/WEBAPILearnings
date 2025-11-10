b4af6576600fdbba567b5874c8de2e1b52026800
Kiran <kiran.wilson@gmail.com>
2025-11-10 11:17:38 +0400

    Add files via upload

---
+++ Interfaces/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WarrantyAPITest.Models;
+
+namespace WarrantyAPITest.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> SignUpAsync(SignUp signupModel);
+
+    }
+}
