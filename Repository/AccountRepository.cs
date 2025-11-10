8f077a9c12b704b6f30cf98386bb8d6e790b8b00
Kiran <kiran.wilson@gmail.com>
2025-11-10 11:16:05 +0400

    Add files via upload

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WarrantyAPITest.Interfaces;
+using WarrantyAPITest.Models;
+
+namespace WarrantyAPITest.Repository
+{
+    public class AccountRepository : IAccountRepository
+
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IdentityResult> SignUpAsync(SignUp signupModel)
+        {
+            var user = new ApplicationUser()
+            {
+                FirstName = signupModel.FirstName,
+                SecondName = signupModel.SecondName,
+                Email = signupModel.Email,
+                UserName = signupModel.Email
+            };
+            return await _userManager.CreateAsync(user, signupModel.Password);
+        }
+    }
+}
