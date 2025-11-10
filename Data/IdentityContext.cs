851b8c0ec7fdb752df7998d34bf5c814998ddcc0
Kiran <kiran.wilson@gmail.com>
2025-11-10 11:18:22 +0400

    Add files via upload

---
+++ Data/IdentityContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WarrantyAPITest.Models;
+
+namespace WarrantyAPITest.Data
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+        : base(options)
+        {
+        }
+
+      //  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      //=> optionsBuilder.UseSqlServer("Name=ConnectionStrings:IdentityDbConnection");
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder); // MUST call base to configure Identity keys
+        }
+
+    }
+}
