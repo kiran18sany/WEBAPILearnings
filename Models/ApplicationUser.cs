4fb6e18a8ddae301c7cb7944ea1cfc97b3cb2725
Kiran <kiran.wilson@gmail.com>
2025-11-10 11:16:58 +0400

    Add files via upload

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WarrantyAPITest.Models
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+
+}
---
+++ Models/SignUp.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WarrantyAPITest.Models
+{
+    public class SignUp
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string SecondName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]      
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
