c0e3ed49cda3c9176605a87180574f4341a966fa
Kiran <kiran.wilson@gmail.com>
2025-11-12 11:59:39 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,7 +2,14 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using WarrantyAPITest.Data;
 using WarrantyAPITest.Interfaces;
 using WarrantyAPITest.Repository;
+using WarrantyAPITest.Helper;
 using AutoMapper;
+using WarrantyAPITest.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +19,52 @@ builder.Services.AddControllers();
 
 
 builder.Services.AddTransient<IEwclaimPartMasterRepository, EwclaimPartMasterRepository>();
+builder.Services.AddTransient<IAccountRepository, AccountRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<GaponlineDemo1Context>();
+//builder.Services.AddDbContext<GaponlineDemo1Context>();
+builder.Services.AddDbContext<GaponlineDemo1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddDbContext<IdentityContext>();
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnection")));
+
+//This is added for Identity /Security things.
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.SaveToken = true;
+    option.RequireHttpsMetadata = false;
+    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
+
+
+
 builder.Services.AddControllers().AddNewtonsoftJson();
-builder.Services.AddAutoMapper(typeof(Program)); //(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(typeof(ApplicationMapper)); //(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -33,7 +78,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors();
 app.MapControllers();
 
 app.Run();
